5712cf7133fefc8cc1e8d5d0a11c04aa5caa7b65
Mustafa Sibai <contact@m-sibai.com>
2021-03-18 15:18:38 +0400

    fixed env file

--- env.js
+++ env.js
@@ -1,9 +1,9 @@
 const runEnv = () => {
     if (process.argv[2] == '--dev') {
-        require('dotenv').config({ path: `./.env_dev` })
+        require('dotenv').config({ path: `./dev.env` })
     }
     else if (process.argv[2] == '--prod') {
-        require('dotenv').config({ path: `./.env_prod` })
+        require('dotenv').config({ path: `./prod.env` })
     }
 }
 
